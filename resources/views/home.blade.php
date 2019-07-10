3abc6024a597a76c210791aa03fb630229f890d6
Joanna Łukaszek <joanna.lukszek@euvic.pl>
2019-07-10 17:57:48 +0200

    Create simple layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,33 @@
 
 @section('content')
 <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">Dashboard</div>
-
-        <div class="card-body">
-          @if (session('status'))
-            <div class="alert alert-success" role="alert">
-              {{ session('status') }}
-            </div>
-          @endif
-
-          You are logged in!
-        </div>
+  <div class="row">
+    <div class="col-3 d-flex justify-content-center">
+      <img src="/svg/logo-color.svg" alt="Logo" class="rounded-circle" />
+    </div>
+    <div class="col-9">
+      <div class="div">
+        <h2>somebatstuff</h2>
       </div>
+      <div class="d-flex">
+        <div class="pr-5"><strong>154</strong> posts</div>
+        <div class="pr-5"><strong>23k</strong> followers</div>
+        <div class="pr-5"><strong>231</strong> following</div>
+      </div>
+      <div class="pt-4 font-weight-bold">someBatStuff.org</div>
+      <div>Commodo elit at imperdiet dui accumsan sit amet. Mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing.</div>
+      <div><a href="#">somebatstuff.com</a></div>
+    </div>
+  </div>
+  <div class="row pt-5">
+    <div class="col-4">
+      <img class="w-100" src="https://scontent-waw1-1.cdninstagram.com/vp/493e4d0abe54de2a03f9723a3d578316/5DBAF0A1/t51.2885-15/sh0.08/e35/c0.109.925.925a/s640x640/66170854_402202580395287_1871271487333362373_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com" alt="">
+    </div>
+    <div class="col-4">
+      <img class="w-100" src="https://scontent-waw1-1.cdninstagram.com/vp/94cacfdca9d64c08090d75cb9d8345e7/5DBD4D94/t51.2885-15/sh0.08/e35/c6.0.737.737a/s640x640/64842736_675454446259763_390781940470065216_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com" alt="">
+    </div>
+    <div class="col-4">
+      <img class="w-100" src="https://scontent-waw1-1.cdninstagram.com/vp/e1e7dafc1b9e254766d03f4364b43d51/5DB2D24B/t51.2885-15/sh0.08/e35/c0.74.877.877a/s640x640/61129659_484895502256050_2457635576497186056_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com" alt="">
     </div>
   </div>
 </div>
